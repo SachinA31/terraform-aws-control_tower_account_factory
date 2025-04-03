86239e3664cd156794e1fb8134d31bfd06c53bb9
SachinA31 <sachinargade111@gmail.com>
2025-04-04 00:04:03 +0530

    chnages

--- examples/github+tf_oss/main.tf
+++ examples/github+tf_oss/main.tf
@@ -2,18 +2,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source = "./modules"
   # Required Vars
-  ct_management_account_id    = "111122223333"
-  log_archive_account_id      = "444455556666"
-  audit_account_id            = "123456789012"
-  aft_management_account_id   = "777788889999"
+  ct_management_account_id    = "559050210666"
+  log_archive_account_id      = "296062575241"
+  audit_account_id            = "746669199180"
+  aft_management_account_id   = "050451399214"
   ct_home_region              = "us-east-1"
   tf_backend_secondary_region = "us-west-2"
   # VCS Vars
-  vcs_provider                                  = "github"
-  account_request_repo_name                     = "ExampleOrg/example-repo-1"
-  global_customizations_repo_name               = "ExampleOrg/example-repo-2"
-  account_customizations_repo_name              = "ExampleOrg/example-repo-3"
-  account_provisioning_customizations_repo_name = "ExampleOrg/example-repo-4"
+  #vcs_provider                                  = "github"
+  #account_request_repo_name                     = "ExampleOrg/example-repo-1"
+  #global_customizations_repo_name               = "ExampleOrg/example-repo-2"
+  #account_customizations_repo_name              = "ExampleOrg/example-repo-3"
+  #account_provisioning_customizations_repo_name = "ExampleOrg/example-repo-4"
 }
